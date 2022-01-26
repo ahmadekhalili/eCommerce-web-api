c3c8b0bdfa1cee436df22d46735e302b94eb7c13
ahmadekhalili <ahmadkhalili2020@gmail.com>
2022-01-26 15:44:42 +0330

    fix user create_user

--- mrgol/users/models.py
+++ mrgol/users/models.py
@@ -34,7 +34,7 @@ class UserManager(BaseUserManager):
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(phone, password1, password2, **extra_fields)
+        return self._create_user(phone, password, **extra_fields)
 
     def create_superuser(self, phone, password, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
