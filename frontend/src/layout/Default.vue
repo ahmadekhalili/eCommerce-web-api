80810c5f39e2074d3a9573355b14602509616e73
mkh <mohamadekhalili100100@gmail.com>
2023-01-20 22:10:46 +0330

    add en and fa i18n for footer elements

--- frontend/src/layout/Default.vue
+++ frontend/src/layout/Default.vue
@@ -3,18 +3,12 @@
   <v-app class="bg-surface" style="width: 100vw;">
     <v-locale-provider :rtl="$t('base.direction') === 'RTL'">
       <v-hover v-slot="{ isHovering, props }">
-        <header v-bind="props" class="b-glass" :style="`
-         transition: background-color 1s;
-         -webkit-transition: background-color 1s;
-         min-height:74px;
-         position: fixed;
-         z-index: 3; 
-         width: 100%;
+        <header v-bind="props" class="b-glass header-default" :style="`
          background-color: ${(isHovering || !topHideBackgroundHeader) && $vuetify?.theme?.current?.colors?.background ?
     $vuetify?.theme?.current?.colors?.background + 'ff' :
     $vuetify?.theme?.current?.colors?.background + $vuetify?.theme?.current?.variables?.alpha1};
         `">
-          <section class=" d-flex flex-row" style="height: 74px">
+          <section class="d-flex flex-row" style="height: 74px">
             <!-- logo -->
             <DefaultLayoutLogo />
             <!-- space  -->
@@ -45,6 +39,7 @@
         </header>
       </v-hover>
       <slot />
+      <FooterDefault />
     </v-locale-provider>
   </v-app>
 </template>
@@ -57,8 +52,20 @@ import HeaderMenu from "./components/menu/HeaderMenu.vue";
 import DefaultLayoutLogo from "./components/header/DefaultLayoutLogo.vue";
 import DefaultLayoutUserSign from "./components/header/DefaultLayoutUserSign.vue";
 import DefaultLayoutCart from "./components/header/defaultLayoutCart.vue";
+import FooterDefault from "./components/footer/FooterDefault.vue";
 
 withDefaults(defineProps<Props>(), {
   topHideBackgroundHeader: false
 })
 </script>
+
+<style scoped>
+.header-default {
+  transition: background-color 1s;
+  -webkit-transition: background-color 1s;
+  min-height: 74px;
+  position: fixed;
+  z-index: 3;
+  width: 100%;
+}
+</style>
---
+++ frontend/src/layout/i18n/fa.ts
@@ -0,0 +1,26 @@
+import type { default as i18n, footer as footerType } from "./../models/i18n";
+
+const footer = (): footerType => {
+  return {
+    "about-us": "درباره ما",
+    "about-us-description":
+      "گروه SMELLSHOP  آماده ارائه خدمات مشاوره و فروش در حوزه عطر و ادکلن می باشد.",
+    "contact-us": "تماس با ما",
+    "new-fragrances": "عطرهای جدید",
+    "privacy-policy": "سیاست حفظ حریم خصوصی",
+    "product-specifications": "مشخصات محصول",
+    "return-policy": "سیاست بازگشت",
+    "terms-and-conditions": "شرایط و ضوابط",
+    "useful-links": "لینک های مفید",
+    newsletter: "خبرنامه",
+    subscribe: "اشتراک",
+    "subscribe-description":
+      "برای دریافت به‌روزرسانی‌ها، دسترسی به معاملات انحصاری و موارد دیگر مشترک شوید.",
+    "enter-your-email-address": "آدرس ایمیل خود را وارد کنید",
+  };
+};
+const i18n = (): i18n => {
+  return {
+    footer: footer(),
+  };
+};
