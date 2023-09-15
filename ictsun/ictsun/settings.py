a6d2663b6be7ce02e04576908b49a5e2ffaf00dc
ahmadekhalili <ahmadkhalili2020@gmail.com>
2023-09-15 18:46:47 +0330

    Updated environ variables

--- ictsun/ictsun/settings.py
+++ ictsun/ictsun/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-l(24ils3ph2mp0%cguxdq!i%8cfjfldw*1ugu+a@9wk^2x^-e'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
